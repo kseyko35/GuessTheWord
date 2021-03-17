d7408fafd6c09d2212416ae5511c65b6aa9f0343
kseyko35 <seyfiercan35@hotmail.com>
2021-03-17 19:29:33 +0300

    Viewmodel created for scorefragment and Viewmodel factory is created to carry viewmodel's parameter data

---
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.android.guesstheword.screens.score
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+
+/**     Code with ❤
+╔════════════════════════════╗
+║   Created by Seyfi ERCAN   ║
+╠════════════════════════════╣
+║  seyfiercan35@hotmail.com  ║
+╠════════════════════════════╣
+║      17,March,2021      ║
+╚════════════════════════════╝
+ */
+class ScoreViewModel(finalScore: Int) : ViewModel() {
+    // The final score
+    var score = finalScore
+    init {
+        Log.i("ScoreViewModel", "Final score is $finalScore")
+    }
+}
---
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -0,0 +1,24 @@
+package com.example.android.guesstheword.screens.score
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+
+/**     Code with ❤
+╔════════════════════════════╗
+║   Created by Seyfi ERCAN   ║
+╠════════════════════════════╣
+║  seyfiercan35@hotmail.com  ║
+╠════════════════════════════╣
+║      17,March,2021      ║
+╚════════════════════════════╝
+ */
+
+class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
+            return ScoreViewModel(finalScore) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
